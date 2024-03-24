c930b1da4ac3848a1cff8e463e45b0bdf95f6c55
David Liebherr <ambition.aidev@gmail.com>
2024-03-24 15:08:42 -0500

    leaned about importing enums

--- rps/rps.py
+++ rps/rps.py
@@ -1,8 +1,14 @@
 import sys
 import random
+from enum import Enum
+
+class RPS(Enum):
+   ROCK = 1
+   PAPER = 2
+   SCISSORS = 3
+
 
 print('')
-
 player_choice = input("Enter...\n1 for rock,\n2 for paper,\n3 for scissors:\n\n")
 player = int(player_choice)
 
@@ -10,23 +16,23 @@ if player < 1 or player > 3:
    sys.exit("You must enter 1, 2 or 3...")
 
 ai_choice = random.choice('123')
-
 system = int(ai_choice)
+
 print('')
-print('You chose ' + player_choice + '!')
-print('Python chose ' + ai_choice + '!')
+print('You chose ' + str(RPS(player)).replace('RPS.', '') + '!')
+print('Python chose ' + str(RPS(system)).replace('RPS.', '') + '!')
 print('')
 
 if player == 1 and system == 3:
-   print('You Won')
+   print('🎉 You Won')
 elif player == 2 and system == 1:
-   print('You Won')
+   print('🎉 You Won')
 elif player == 3 and system == 2:
-   print('You Won')
+   print('🎉 You Won')
 elif player == system:
-   print("Tie")
+   print("😶 Tie")
 else:
-   print('Python Won!')
+   print('🐍 Python Won!')
    
 
 
