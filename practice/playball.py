968ecc85698fff5cfc67562fdc640bce4a61319c
David Liebherr <ambition.aidev@gmail.com>
2024-03-29 08:39:12 -0500

    Warmup code - practice

---
+++ practice/playball.py
@@ -0,0 +1,57 @@
+import random
+print('')
+
+count = {
+   'balls': 2,
+   'strikes': 2,
+   'outs': 2
+}
+
+team = {
+   'home': "Brewers",
+   'vistiors': "Reds",
+}
+
+atBat = team['home']
+
+def switchAtBat():
+   global atBat
+   if atBat == team['home']:
+      atBat = team['vistiors']
+
+def reset_current_count():
+   count['balls'] = 0
+   count['strikes'] = 0
+
+def reset_count():
+   count['balls'] = 0
+   count['strikes'] = 0
+   count['outs'] = 0
+
+rolled_dice = random.randint(1,6)
+print(f"Dice result: {rolled_dice}") # Print
+
+
+if rolled_dice == 1 or rolled_dice == 3 or rolled_dice == 5:
+   count['balls'] += 1
+   if count['balls'] == 4:
+      reset_current_count()
+   call = f'Balls: {count["balls"]}\nStrikes: {count["strikes"]}\nOuts: {count["outs"]}'
+   print("Ball")
+   print("")   
+   print(call) # Print
+else:
+   count['strikes'] += 1
+   if count['strikes'] == 3:
+      count['outs'] +=1
+      reset_current_count()
+      if count['outs'] == 3:
+         reset_count()
+         switchAtBat()
+   call = f'Balls: {count["balls"]}\nStrikes: {count["strikes"]}\nOuts: {count["outs"]}'
+   print("Strike")
+   print("")   
+   print(call) # Print
+   
+print('')
+print(atBat) # Print
